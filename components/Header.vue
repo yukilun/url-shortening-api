b14f86dd88f40af8034e6133119baea895f02ffa
yukilun <102889935+yukilun@users.noreply.github.com>
2023-10-04 04:09:13 -0700

    update z-index

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-[90%] max-w-[1110px] mx-auto pt-10 pb-5 md:pb-10 flex justify-between items-center gap-10 relative">
+    <header class="w-[90%] max-w-[1110px] mx-auto pt-10 pb-5 md:pb-10 flex justify-between items-center gap-10 relative z-10">
         <IconLogo fillColor="#34313D"/>
         <HeaderNav class="hidden lg:flex" />
         <button type="button" class="block p-3 lg:hidden" aria-label="open-menu" @click="toggleNavMenu">
