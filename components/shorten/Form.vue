05bca9f6fadca6ce5aba8b25894b81bab4a96a0b
yukilun <102889935+yukilun@users.noreply.github.com>
2023-10-04 01:30:43 -0700

    display error message

--- components/shorten/Form.vue
+++ components/shorten/Form.vue
@@ -4,8 +4,10 @@ import LogoVue from '../icon/Logo.vue';
     <form @submit="handleSubmit" class="absolute top-1/2 -translate-y-1/2 left-[5%] w-[90%] flex flex-col md:flex-row justify-between md:items-center md:gap-5 text-lg md:text-xl">
         <input type="text" placeholder="Shorten a link here" aria-label="url-to-be-shorten" v-model="inputUrl" @input="() => handleInput()"
             class="px-4 py-3 md:px-6 md:py-4 md:flex-grow rounded-lg outline-offset-[3px]" 
-            :class='isEmpty && "border-[3px] border-red"'>
-        <p v-if="isEmpty" class="text-base text-red italic md:absolute md:top-full pt-1">Please add a link</p>
+            :class='(isEmpty || errorMsg) && "border-[3px] border-red"'>
+        <p v-if="isEmpty || errorMsg" class="text-base text-red italic md:absolute md:top-full pt-1">
+            {{errorMsg ? errorMsg : 'Please add a link'}}
+        </p>
         <div class="bg-white rounded-lg overflow-hidden h-fit w-auto mt-4 md:mt-0">
             <button type="submit" class="w-full text-white bg-cyan font-bold py-3 md:px-9 md:py-4 transition-all duration-300 hover:opacity-50">
                 Shorten it!
@@ -20,6 +22,7 @@ const linksStore = useLinksStore();
 
 const inputUrl = ref('');
 const isEmpty = ref(false);
+const errorMsg = ref('');
 
 function handleInput() {
     isEmpty.value = inputUrl.value == ''; 
@@ -30,11 +33,16 @@ async function handleSubmit(e) {
         isEmpty.value = true;
         return;
     }  
-    const { data } = await useFetch('/api', {
+    const { data, error } = await useFetch('/api', {
         method: 'POST',
         body: {url: inputUrl.value}
     });
-    linksStore.addLink(data.value.url, data.value.token);
+    if(error.value) {
+        errorMsg.value = error.value.data.statusMessage;
+    }
+    else {
+        linksStore.addLink(data.value.url, data.value.token);
+    }
 }
 
 </script>
